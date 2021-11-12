230cf28887a5c7023f9f4a47bc222374cd131eb4
Wyatt <wkirschn@uoguelph.ca>
2021-11-11 22:36:37 -0500

    Before Adding Cloud Firestore

--- app/src/main/java/com/example/f21comp3025resturantrater/MainActivity.kt
+++ app/src/main/java/com/example/f21comp3025resturantrater/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.f21comp3025resturantrater
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.f21comp3025resturantrater.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addResturantButton.setOnClickListener {
+            if (binding.inputResturantName.text.toString()
+                    .isNotEmpty() && binding.ratingSpinner.selectedItemPosition > 0
+            ) {
+                // Store Firebase Connection
+
+                Toast.makeText(this, "Restaurant name and rating added!", Toast.LENGTH_LONG)
+                    .show()
+
+
+            } else {
+                Toast.makeText(this, "Restaurant name and rating required!", Toast.LENGTH_LONG)
+                    .show()
+            }
+
+
+        }
     }
 }
