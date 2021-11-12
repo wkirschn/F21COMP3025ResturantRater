f971bbc3f9bbf6fe7ec67256e62e998d29350bba
Wyatt <wkirschn@uoguelph.ca>
2021-11-12 02:22:23 -0500

    Fetching Completed

--- app/src/main/java/com/example/f21comp3025resturantrater/RestauratListActivity.kt
+++ app/src/main/java/com/example/f21comp3025resturantrater/RestauratListActivity.kt
@@ -3,6 +3,7 @@ package com.example.f21comp3025resturantrater
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import com.example.f21comp3025resturantrater.databinding.ActivityRestauratListBinding
 import com.google.firebase.firestore.FirebaseFirestore
 
@@ -17,7 +18,7 @@ class RestauratListActivity : AppCompatActivity() {
 
         // Connect to the DB
 
-        val db = FirebaseFirestore.getInstance().collection("restaurants")
+        val db = FirebaseFirestore.getInstance().collection("resturants")
 
         val query = db.get().addOnSuccessListener {
             documents ->
@@ -26,6 +27,18 @@ class RestauratListActivity : AppCompatActivity() {
             for (document in documents)
             {
                 Log.i("DB Response", "${document.data}")
+
+                //create a restaurant object from the DB
+
+                val restaurant = document.toObject((Restaurant::class.java))
+
+
+                val textView = TextView(this)
+                textView.text = restaurant.name + " " + restaurant.rating.toString()
+
+                textView.textSize = 20f
+
+                binding.layoutGridFetch.addView(textView)
             }
         }
 
