8cd26f4b6aa2523f398cf4f4c87ab98bfd55dbba
Wyatt <wkirschn@uoguelph.ca>
2021-11-13 02:52:35 -0500

    Building the Last Adapter

---
+++ app/src/main/java/com/example/f21comp3025resturantrater/Comment.kt
@@ -0,0 +1,14 @@
+package com.example.f21comp3025resturantrater
+
+class Comment (var commentID: String?=null,
+               var userName : String?=null,
+                var comment : String?=null,
+                var restaurantID : String?=null)
+
+
+{
+
+
+
+
+}
--- app/src/main/java/com/example/f21comp3025resturantrater/CommentActivity.kt
+++ app/src/main/java/com/example/f21comp3025resturantrater/CommentActivity.kt
@@ -2,7 +2,9 @@ package com.example.f21comp3025resturantrater
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.f21comp3025resturantrater.databinding.ActivityCommentBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class CommentActivity : AppCompatActivity() {
 
@@ -14,5 +16,32 @@ class CommentActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.restaurantNameTextView.text = intent.getStringExtra("restaurantName")
+
+
+        binding.saveCommentButton.setOnClickListener {
+
+            val userName = binding.usernameEditText.text.toString()
+            val commentBody = binding.bodyEditText.text.toString()
+
+
+            // create the ability to save a comment
+            if (userName.isNotEmpty() && commentBody.isNotEmpty())
+            {
+                val db = FirebaseFirestore.getInstance().collection("comments")
+                val id = db.document().id
+
+                val restaurantID = intent.getStringExtra("restaurantID")
+                restaurantID?.let {
+                    val newComment = Comment(id, userName, commentBody, restaurantID )
+                }
+            }
+            else
+            {
+                Toast.makeText(this,"Both user ame and comment are required", Toast.LENGTH_LONG).show()
+            }
+        }
+
+
+
     }
 }
